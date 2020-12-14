255217d92789ec4351b7d9430e854d25fbdcc901
wsork <wsork@163.com>
2020-12-15 02:05:50 +0800

    初始搭建基础工程12.15

---
+++ cupdata-pms/src/main/java/com/cupdata/pms/CupdataPmsApplication.java
@@ -0,0 +1,13 @@
+package com.cupdata.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupdataPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupdataPmsApplication.class, args);
+	}
+
+}
---
+++ cupdata-pms/src/test/java/com/cupdata/pms/CupdataPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupdata.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupdataPmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
