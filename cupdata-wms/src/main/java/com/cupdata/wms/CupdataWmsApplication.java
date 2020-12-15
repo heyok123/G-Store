125a274d40b91ed6298b3172fd5eee00c48a8575
wsork <wsork@163.com>
2020-12-15 11:14:23 +0800

    wms基础服务搭建 12.15.4

---
+++ cupdata-wms/src/main/java/com/cupdata/wms/CupdataWmsApplication.java
@@ -0,0 +1,13 @@
+package com.cupdata.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupdataWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupdataWmsApplication.class, args);
+	}
+
+}
---
+++ cupdata-wms/src/test/java/com/cupdata/wms/CupdataWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupdata.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupdataWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
